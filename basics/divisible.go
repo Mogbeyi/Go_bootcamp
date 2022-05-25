7135909237ad58da5ddb21e01fff878ef81e7d2b
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-05-26 00:23:26 +0100

    Solve divisible

---
+++ basics/divisible.go
@@ -0,0 +1,23 @@
+package main 
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arg := os.Args[1]
+	num, err := strconv.Atoi(arg)
+	if err != nil {
+		fmt.Printf("%v is not a number", arg)
+	}
+
+	if num % 2 != 0 {
+		fmt.Printf("%d is an odd number", num)
+	} else if num % 2 == 0 && num % 8 == 0 {
+		fmt.Printf("%d is an even number and is divisible by 8", num)
+	} else {
+		fmt.Printf("%d is an even number", num)
+	}
+}
