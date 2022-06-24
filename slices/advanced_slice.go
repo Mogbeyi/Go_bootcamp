6ba54ab140e95aaec304100313f22b3b04f5e5f1
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-25 00:20:12 +0100

    Add 6th step

--- slices/advanced_slice.go
+++ slices/advanced_slice.go
@@ -23,5 +23,14 @@ func main() {
 	s.Show("4th step", names)
 
 	clones := make([]string, 3, 5)
+	copy(clones, names[2:])
 	s.Show("5th step", clones)
+	clones = append(clones, names[:2]...)
+	s.Show("5th step", clones)
+
+	sliced := clones[1:5]
+	sliced = append(sliced, "hypatia")
+	s.Show("6th step", sliced)
+	clones[2] = "elder"
+	s.Show("6th step", clones, sliced)
 }
