eb241f82d4fe6d5244f040b2b2be7f776b2c957d
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-07-07 20:21:15 +0100

    Mask the links

--- slices/spam_masker.go
+++ slices/spam_masker.go
@@ -15,19 +15,35 @@ func main() {
 	const (
 		link = "https://"
 		nlink = len(link)
+		mask = '*'
 	)
 
 	var (
 		text = args[0]
 		size = len(text)
 		buf = make([]byte, 0, size)
+		in bool
 	)
 
 	for i := 0; i < size; i++ {
-		buf = append(buf, text[i])
-
 		if len(text[i:]) >= nlink && text[i:i+nlink] == link {
-			fmt.Println(text[i : i + nlink])
+			in = true
+			buf = append(buf, link...)
+			i += nlink
 		}
+
+		c := text[i]
+
+		switch c {
+			case ' ', '\t', '\n':
+				in = false
+		}
+		
+		if in {
+			c = mask 
+		}
+
+		buf = append(buf, c)
 	}
+	fmt.Println(string(buf))
 }
