2ff1f0a04d77f16582a1102d41b53d06274aad71
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-07-27 19:24:28 +0100

    Complete command line game

--- struct/command_line_game.go
+++ struct/command_line_game.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	type item struct {
@@ -23,11 +30,89 @@ func main() {
 		{item: item{id: 3, name: "minecraft", price: 20}, genre: "sandbox"},
 	}
 
+	//index the games by ID
+	byID := make(map[int]game)
+	for _, g := range games {
+		byID[g.id] = g
+	}
+
 	fmt.Printf("Emmy's game store has %d games.\n\n", len(games))
 
-	for _, g := range games {
-		fmt.Printf("#%d: %-15q %-20s $%d\n",
-			g.id, g.name, "("+g.genre+")", g.price)
+	in := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf(`
+  > list   : lists all the games
+  > id N   : queries a game by id
+  > save   : exports the data to json and quits
+  > quit   : quits
+`)
+				if !in.Scan() {
+					break
+				}			
+
+				fmt.Println()
+
+				cmd := strings.Fields(in.Text())
+				if len(cmd) == 0 {
+					continue
+				}
+
+				switch cmd[0]{
+				case "quit":
+					fmt.Println("bye!")
+					return
+
+				case "list":
+					for _, g := range games {
+						fmt.Printf("#%d: %-15q %-20s $%d\n",
+							g.id, g.name, "("+g.genre+")", g.price)
+					}
+
+				case "id":
+					if len(cmd) != 2 {
+						fmt.Println("Wrong Id")
+						continue
+					}
+
+					id, err := strconv.Atoi(cmd[1])
+					if err != nil {
+						fmt.Println("Wrong id")
+						continue
+					}
+
+					g, ok := byID[id]
+					if !ok {
+						fmt.Println("Sorry. I don't have the game")
+						continue
+					}
+
+					fmt.Printf("#%d: %-15q %-20s $%d\n",
+						g.id, g.name, "("+g.genre+")", g.price)
+
+				case "save":
+					type jsonGame struct {
+						ID 	  int	 `json:"id"`
+						Name  string `json:"name"`
+						Genre string `json:"genre"`
+						Price int 	 `json:"price"`
+					}
+
+					// load the data into the encodable game values
+					var encodable []jsonGame
+					for _, g := range games {
+						encodable = append(encodable, 
+							jsonGame{g.id, g.name, g.genre, g.price})
+					}
+
+					out, err := json.MarshalIndent(encodable, "", "\t")
+					if err != nil {
+						fmt.Println("Sorry:", err)
+						continue
+					}
+
+					fmt.Println(string(out))
+					return
+				}
 	}
-	
 }
