a3dc275651fba471bfe541865db8047a7c26f4dd
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2023-03-13 16:24:18 +0100

    Solve area of a circle

--- struct/circle.go
+++ struct/circle.go
@@ -9,13 +9,13 @@ type Circle struct {
 	x, y, r float64
 }
 
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
 
 func main() {
 	c := Circle{x: 4, y: 9, r: 2}
 
-	fmt.Println(calcArea(c))
+	fmt.Println(c.area())
 }
 
-func calcArea(c Circle) float64 {
-	return math.Pi * c.r * c.r
-}
